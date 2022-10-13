716738637134063ced2aacec6d1a86e5f4f28aa3
Sus2007 <ntimsusan8@gmail.com>
2022-10-13 08:14:07 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-
 #include <stdio.h>
 /**
 * main -Entry point
 *
-Return: Always 0 nt
+* Return: Always 0 nt
 */
-int main()
-  puts("\"Prograng is like building a multilingual puzzle");
-return (0)ways 0
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 
 }
